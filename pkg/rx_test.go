bbeb9802d65054eeb660b21ee4e3f1c8f60614ce
Jason Yu <onionf91@gmail.com>
2022-01-16 21:20:25 +0800

    Test concurrent processes with rxgo

--- pkg/rx_test.go
+++ pkg/rx_test.go
@@ -1,11 +1,14 @@
 package pkg
 
 import (
+	"context"
 	"fmt"
 	"github.com/reactivex/rxgo/v2"
 	"log"
+	"math/rand"
 	"syscall"
 	"testing"
+	"time"
 )
 
 func logMsg(format string, v ...interface{}) {
@@ -24,4 +27,19 @@ func TestJust(t *testing.T) {
 	if item.V != msg {
 		t.Errorf("got %v, wanted %v", item.V, msg)
 	}
+}
+
+func TestConcurrentProcess(t *testing.T) {
+	observable := rxgo.Range(1, 4).FlatMap(func(i rxgo.Item) rxgo.Observable {
+		logMsg("proc %v", i.V)
+		return rxgo.Range(1, 4).Map(func(_ context.Context, v interface{}) (interface{}, error) {
+			logMsg("task %v emit %v", i.V, v)
+			//time.Sleep(time.Millisecond * time.Duration(rand.Intn(200)))
+			time.Sleep(time.Second * time.Duration(rand.Intn(5)))
+			return fmt.Sprintf("task %v emit %v", i.V, v), nil
+		}, rxgo.WithPool(4))
+	}, rxgo.WithBufferedChannel(4)).DoOnNext(func(s interface{}) {
+		logMsg("~~~ %v", s)
+	})
+	<-observable
 }
